94cf02356a6a0f309217cde2032bdf2302cfdfa5
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2025-06-16 09:31:47 -0400

    feat: intial project setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Tabs } from 'expo-router';
+
+const TabLayout = () => {
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="Profile"
+				options={{
+					title: 'Profile',
+					tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+				}}
+			/>
+		</Tabs>
+	);
+};
+
+export default TabLayout;
