5f1f27db6143e5f3dccbd189294f34d2d839ccf4
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2025-06-23 10:46:53 -0400

    update: rm comment

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -16,8 +16,8 @@ const Login = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',  // vertically center
-		alignItems: 'center',      // horizontally center
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 	text: {
 		fontSize: 24,
